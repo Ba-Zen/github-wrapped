7e2645e25fe9b580fb331ce17c42bc99d1b0b527
Ba-Zen <bazenatlas@gmail.com>
2020-01-23 15:03:14 -0500

    gets single user

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Howdy Meng!</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userData: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/ba-zen`).then(response => {
+      console.log('anything here??', response.data);
+      this.setState({ userData: response.data });
+    });
+  }
+
+  render() {
+    const { userData } = this.state;
+    console.log(userData);
+    return (
+      <div className='App'>
+        <h1>Howdy Meng!</h1>
+        <h4>{userData.login}</h4>
+      </div>
+    );
+  }
 }
-
 export default App;
